707cbcefbb12217a763301453f8062e0e6544cfe
Julien Calixte <julienc@bam.tech>
2023-04-06 23:04:55 +0200

    init NewTaskForm

---
+++ src/use-cases/task/components/NewTaskForm.test.ts
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import NewTaskVue from './NewTaskForm.vue'
+
+describe('New Task Form', () => {
+  it('displays New Task Form title', () => {
+    const wrapper = mount(NewTaskVue)
+
+    expect(wrapper.text()).toContain('New Task Form')
+  })
+})
---
+++ src/use-cases/task/components/NewTaskForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>New Task Form</h1>
+    <form @submit.prevent>
+      <label for="title">Title</label>
+      <input type="text" id="title" />
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
