4829a3cff4ca057dcbbdfd2591f9de736d8920b5
Julien Calixte <julienc@bam.tech>
2023-04-06 23:04:50 +0200

    sort imports

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,6 +1,5 @@
-import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
 import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld', () => {
