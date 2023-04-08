812e7509d5506f5cd25f1e215342ef975337ea66
Julien Calixte <julienc@bam.tech>
2023-04-08 12:24:52 +0200

    init new route for a new task

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,10 +12,12 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/new-task',
+      name: 'new-task',
+      component: () => import('../views/task/NewTask.vue')
     }
   ]
 })
---
+++ src/views/task/NewTask.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NewTaskForm from '@/use-cases/task/components/NewTaskForm.vue'
+</script>
+
+<template>
+  <div class="new-task">
+    <NewTaskForm />
+  </div>
+</template>
+
+<style scoped>
+.new-task {
+}
+</style>
