e098aa006cf23b94cd75c45146271528c9ce49fa
Julien Calixte <julienc@bam.tech>
2023-04-09 11:36:56 +0200

    no default import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { router } from './router'
 
 import './assets/main.css'
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -27,5 +27,3 @@ const router = createRouter({
     }
   ]
 })
-
-export default router
