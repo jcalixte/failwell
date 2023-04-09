19c9ea8002ae9ae2f9270ead83f0a0f8f7c697f3
Julien Calixte <julienc@bam.tech>
2023-04-09 10:33:44 +0200

    reset store facilities

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import TaskList from '@/use-cases/task/components/TaskList.vue'
+import { useTaskStore } from '@/use-cases/task/stores/useTask.store'
+
+const taskStore = useTaskStore()
 </script>
 
 <template>
   <main>
     <RouterLink :to="{ name: 'new-task' }">New task</RouterLink>
     <task-list />
+    <button @click="() => taskStore.reset()">reset list</button>
   </main>
 </template>
