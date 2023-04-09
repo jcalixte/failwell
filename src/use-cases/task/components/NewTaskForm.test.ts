ef20813c664da697c4fda29705dcd81813bc1aa9
Julien Calixte <julienc@bam.tech>
2023-04-09 10:47:26 +0200

    add a store for newTaskForm test

--- src/use-cases/task/components/NewTaskForm.test.ts
+++ src/use-cases/task/components/NewTaskForm.test.ts
@@ -1,10 +1,11 @@
+import { withStore } from '@/tests/utils'
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 import NewTaskVue from './NewTaskForm.vue'
 
 describe('New Task Form', () => {
   it('displays New Task Form title', () => {
-    const wrapper = mount(NewTaskVue)
+    const wrapper = mount(NewTaskVue, withStore())
 
     expect(wrapper.text()).toContain('New Task Form')
   })
