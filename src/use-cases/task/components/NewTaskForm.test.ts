5719fabac192344456caec957a267cdf3078c0f7
Julien Calixte <julienc@bam.tech>
2023-04-09 11:37:25 +0200

    fix test with router and new text

--- src/use-cases/task/components/NewTaskForm.test.ts
+++ src/use-cases/task/components/NewTaskForm.test.ts
@@ -1,12 +1,12 @@
-import { withStore } from '@/tests/utils'
+import { withPlugins } from '@/tests/utils'
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 import NewTaskVue from './NewTaskForm.vue'
 
 describe('New Task Form', () => {
   it('displays New Task Form title', () => {
-    const wrapper = mount(NewTaskVue, withStore())
+    const wrapper = mount(NewTaskVue, withPlugins())
 
-    expect(wrapper.text()).toContain('New Task Form')
+    expect(wrapper.text()).toContain('Create a task')
   })
 })
