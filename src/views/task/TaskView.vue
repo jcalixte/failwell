2bdfd86481c04dca90de3a4d59dde11c796d4039
Julien Calixte <julienc@bam.tech>
2023-04-09 11:02:44 +0200

    display simple task

--- src/views/task/TaskView.vue
+++ src/views/task/TaskView.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-defineProps<{
+import { useTaskStore } from '@/use-cases/task/stores/useTask.store'
+import { computed } from 'vue'
+
+const props = defineProps<{
   id: string
 }>()
+
+const taskStore = useTaskStore()
+
+const task = computed(() => taskStore.getTask(props.id))
 </script>
 
 <template>
-  <div class="task-view"></div>
+  <div class="task-view" v-if="task">
+    {{ task.title }}
+  </div>
+  <div v-else>Task not found</div>
 </template>
 
 <style scoped>
