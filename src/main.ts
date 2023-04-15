6bdf1b260451cba3434fbaa41cb0a16c4316416d
Julien Calixte <julienc@bam.tech>
2023-04-15 14:15:43 +0200

    convert to scss files

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 
-import './assets/main.css'
+import './assets/main.scss'
 
 const app = createApp(App)
 
