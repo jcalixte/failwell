7b0d2d7ae43eab7dccafecc1327257f4b7643775
Julien Calixte <julienc@bam.tech>
2023-04-19 23:45:01 +0200

    add a confirm modal before resetting all tasks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,33 @@ import TaskList from '@/modules/task/components/TaskList.vue'
 import { useTaskStore } from '@/modules/task/stores/useTask.store'
 
 const taskStore = useTaskStore()
+
+const resetTasks = () => {
+  if (window.confirm('are you sure to reset all your tasks?')) {
+    taskStore.reset()
+  }
+}
 </script>
 
 <template>
   <main>
-    <router-link :to="{ name: 'new-task' }" class="button is-primary"
-      >New task</router-link
-    >
+    <div>
+      <router-link :to="{ name: 'new-task' }" class="button is-primary"
+        >New task</router-link
+      >
+    </div>
     <task-list />
-    <button class="button is-danger" @click="() => taskStore.reset()">
-      reset list
-    </button>
+    <div>
+      <button class="button is-danger" @click="resetTasks">reset list</button>
+    </div>
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 60vh;
+}
+</style>
