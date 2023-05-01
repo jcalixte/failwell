c51acf1aaeca24b0174ee8ff8144735c14196f17
Julien Calixte <julienc@bam.tech>
2023-05-01 19:10:24 +0200

    test: 💍 task record
    add test for fromRecordable method

---
+++ src/modules/record/interfaces/time-range.fixture.ts
@@ -0,0 +1,18 @@
+import { toISODate } from '@/shared/types/date'
+import { faker } from '@faker-js/faker'
+import type { TimeRange } from './time-range'
+
+export const createTimeRangeFixture = (
+  partialTimeRange?: Partial<TimeRange>
+): TimeRange => {
+  const [start, end] = faker.date.betweens(
+    toISODate(faker.date.past(1)),
+    toISODate(new Date()),
+    2
+  )
+
+  return {
+    start: partialTimeRange?.start ?? toISODate(start),
+    end: partialTimeRange?.end ?? toISODate(end)
+  }
+}
